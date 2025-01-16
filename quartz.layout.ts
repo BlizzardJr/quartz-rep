a9dfda1453c921ea0cf4cd1eed739051433f1f77
Edin Norman <edinnorman@edins-mbp.home>
2025-01-16 22:32:04 +0100

    Quartz sync: Jan 16, 2025, 10:32 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -60,7 +60,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.Breadcrumbs(),
+    Component.Footer(),
   ],
 }
 
@@ -77,7 +77,7 @@ export const defaultListPageLayout: PageLayout = {
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.Breadcrumbs(),
+    Component.Footer(),
   ],
   
 }
