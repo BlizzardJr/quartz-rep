b24a9a781c4398c183f9e9309647775801de51a9
Edin Norman <edinnorman@edins-mbp.home>
2025-01-17 21:21:09 +0100

    Quartz sync: Jan 17, 2025, 9:21 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -39,7 +39,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#1a1e24",
+          light: "#D3D3D3",
           lightgray: "#1c2532",
           gray: "#922f2f",
           darkgray: "#9da9b5",
