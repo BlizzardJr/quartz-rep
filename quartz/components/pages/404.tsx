a6ea4456a7cfd73b3b10bfe7ca76b235003bff05
Edin Norman <edinnorman@edins-mbp.home>
2025-01-17 19:22:23 +0100

    Quartz sync: Jan 17, 2025, 7:22 PM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -11,7 +11,9 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
-      <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZm50ZXU4MDlucHcwMDV3aW1rdTN5dnY3eWYzNWkyZHlhMTJnN2Z4ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0ExsgrTuACbtPaqQ/giphy.gif"></img>
+      <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZm50ZXU4MDlucHcwMDV3aW1rdTN5dnY3eWYzNWkyZHlhMTJnN2Z4ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0ExsgrTuACbtPaqQ/giphy.gif" 
+       style={{ clear: 'both' }}
+       alt="404 Not Found GIF" />
     </article>
   )
 }
