ba929e144cfea9380b60dce1bb5d6f768fcd33c5
Edin Norman <edinnorman@edins-mbp.home>
2025-01-18 10:27:57 +0100

    Quartz sync: Jan 18, 2025, 10:27 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -14,7 +14,6 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
       <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZm50ZXU4MDlucHcwMDV3aW1rdTN5dnY3eWYzNWkyZHlhMTJnN2Z4ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0ExsgrTuACbtPaqQ/giphy.gif" 
         style={{ display: 'block', marginTop: '20px' }}
        alt="404 Not Found GIF"/>
-
     </article>
   )
 }
